959662575d965881050ea029f37d3adaa9e14947
juanpfrancos <juanpanime@gmail.com>
2020-06-29 14:22:28 -0500

    Casi termino

--- script.js
+++ script.js
@@ -44,6 +44,19 @@ disableOnInteraction: false,
 },
 });
 
+var swiper = new Swiper('.swiper-escuelas', {
+  slidesPerView: 4,
+  spaceBetween: 30,
+  centeredSlides: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+    },
+});
 
 //Whatsapp Form
 
