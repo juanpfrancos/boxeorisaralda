40bcf24eab3c3faaf952190d05e4274802f72e0c
juanpfrancos <juanpanime@gmail.com>
2020-06-30 14:37:27 -0500

    Reparando slides y menu

--- script.js
+++ script.js
@@ -1,23 +1,4 @@
-
-//Swiper Slider
-
-var swiper = new Swiper('.swiper-container', {
-  spaceBetween: 30,
-  effect: 'fade',
-  pagination: {
-    clickable: true,
-  },
-  autoplay: {
-    delay: 2500,
-    disableOnInteraction: false,
-  },
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-});
-
-var swiper = new Swiper('.sw-container', {
+var swiper = new Swiper('.sw-icons', {
   slidesPerView: 1,
   spaceBetween: 5,
   // init: false,
@@ -42,33 +23,4 @@ autoplay: {
 delay: 2500,
 disableOnInteraction: false,
 },
-});
-
-var swiper = new Swiper('.swiper-escuelas', {
-  slidesPerView: 4,
-  spaceBetween: 30,
-  centeredSlides: true,
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
-  autoplay: {
-    delay: 2500,
-    disableOnInteraction: false,
-    },
-});
-
-//Whatsapp Form
-
-
-
-document.querySelector('#submit').addEventListener('click',function(){
-    
-    let name = document.querySelector('#name').value;
-    let email = document.querySelector('#email').value;
-    let message = document.querySelector('#message').value;
-
-    let url = "https://api.whatsapp.com/send?phone=573207678605"+"&text=*_Whatsapp Webform_*%0A%0A*Name:*%0A" + name + "%0A*E-Mail*%0A" + email + "%0A*Message:*%0A" + message;
-    window.open(url);
-
 });
