e311521b8087cecc2fa5e261793cf158b37183c7
juanpfrancos <juanpanime@gmail.com>
2020-07-01 20:51:58 -0500

    Cambios Slider

--- script.js
+++ script.js
@@ -1,28 +1,11 @@
 var swiper = new Swiper('.sw-icons', {
-  slidesPerView: 1,
-  spaceBetween: 1,
-  init: true,
+  spaceBetween: 30,
+  effect: 'fade',
   pagination: {
     clickable: true,
   },
-  breakpoints: {
-    640: {
-      slidesPerView: 2,
-      spaceBetween: 20,
-    },
-    768: {
-      slidesPerView: 4,
-      spaceBetween: 40,
-    },
-    1024: {
-      slidesPerView: 5,
-      spaceBetween: 50,
-    },
-},
-normalizeSlideIndex: true,
-autoplay: {
-delay: 2500,
-disableOnInteraction: false,
-},
-speed: 300,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
 });
